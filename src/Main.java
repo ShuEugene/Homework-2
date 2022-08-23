6629fa9a35bcb82fdc2b2e8da6a31815555890da
ShuEugene <shueugene@hotmail.com>
2022-08-23 18:33:48 +0300

    HW2.2 complete.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,15 @@ public class Main {
         var dog = 8;
         var cat =  3.6;
         var paper = 763789;
-        System.out.println("dog = " + dog + ", cat = " + cat + ", paper = " + paper + ".");
+        System.out.println("\n" +
+                "dog = " + dog + ", cat = " + cat + ", paper = " + paper + ".");
+
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println("\n" +
+                "Значения переменных, после увеличения каждой на 4:" + "\n" +
+                        "dog = " + dog + ", cat = " + cat + ", paper = " + paper + ".");
 
 
     }
